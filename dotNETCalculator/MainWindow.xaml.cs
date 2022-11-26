2e257237b9beed03be4908a1ba7b3f988e490699
Alper Akca <alperakca79@outlook.com>
2022-11-26 20:16:15 +0300

    Add project files.

---
+++ dotNETCalculator/MainWindow.xaml.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string output = "";
+
+        double temp = 0;
+
+        string operation = "";
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Unicode Texts
+            buttonDelete.Content = "\u232B";
+            buttonDivision.Content = "\u00F7";
+            buttonPercent.Content = "\uFF05";
+            buttonAddition.Content = "\u002B";
+            buttonMultiply.Content = "\u00D7";
+            buttonSubtraction.Content = "\u2212";
+            buttonEquals.Content = "\u003D";
+            CopyButton.Content = "📋";
+        }
+
+        // Number Button
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = ((Button)sender).Name;
+
+            switch(name)
+            {
+                case "buttonOne":
+                    output += "1";
+                    Output.Text = output;
+                    break;
+
+                case "buttonTwo":
+                    output += "2";
+                    Output.Text = output;
+                    break;
+
+                case "buttonThree":
+                    output += "3";
+                    Output.Text = output;
+                    break;
+
+                case "buttonFour":
+                    output += "4";
+                    Output.Text = output;
+                    break;
+
+                case "buttonFive":
+                    output += "5";
+                    Output.Text = output;
+                    break;
+
+                case "buttonSix":
+                    output += "6";
+                    Output.Text = output;
+                    break;
+
+                case "buttonSeven":
+                    output += "7";
+                    Output.Text = output;
+                    break;
+
+                case "buttonEight":
+                    output += "8";
+                    Output.Text = output;
+                    break;
+
+                case "buttonNine":
+                    output += "9";
+                    Output.Text = output;
+                    break;
+
+                case "buttonZero":
+                    output += "0";
+                    Output.Text = output;
+                    break;                
+            }
+        }
+
+        // Subtraction Button
+        private void buttonSubtraction_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+
+                output = "";
+
+                operation = "-";
+            }
+            PreviewOutput.Text = (temp.ToString() + " " + operation);
+        }
+
+        // Addition Button
+        private void buttonAddition_Click(object sender, RoutedEventArgs e)
+        {           
+            if (output != "")
+            {
+                temp = double.Parse(output);
+
+                output = "";
+
+                operation = "+";
+            }
+            PreviewOutput.Text = (temp.ToString() + " " + operation);
+        }
+
+        // Multiply Button
+        private void buttonMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+
+                output = "";
+
+                operation = "*";
+            }
+            PreviewOutput.Text = (temp.ToString() + " " + operation);
+        }
+
+        // Division Button
+        private void buttonDivision_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+
+                output = "";
+
+                operation = "/";
+            }
+            PreviewOutput.Text = (temp.ToString() + " " + operation);
+        }
+
+        // Sqr Button
+        private void buttonSqr_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+
+                output = "";
+
+                operation = "sqr";
+            }
+
+            switch (operation)
+            {
+                case "sqr":
+                    double outputTempSqr = temp * temp;
+                    output = outputTempSqr.ToString();
+                    Output.Text = output;
+                    break;
+            }
+            PreviewOutput.Text = operation + "( " + temp + " )";
+        }
+
+        // 1/x Button
+        private void buttonOneX_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != "")
+            {
+                temp = double.Parse(output);
+
+                output = "";
+
+                operation = "1/x";
+            }
+
+            switch (operation)
+            {
+                case "1/x":
+                    double outputTempSqr = (1) / temp;
+                    output = outputTempSqr.ToString();
+                    Output.Text = output;
+                    break;
+            }
+            PreviewOutput.Text = "1/" + "( " +temp + " )";
+        }
+
+        // Equals Button
+        private void buttonEquals_Click(object sender, RoutedEventArgs e)
+        {
+            PreviewOutput.Text = (temp.ToString() + " " + operation + " " + double.Parse(output) + " " + "=");
+
+            switch (operation)
+            {
+                case "-":
+                    double outputTempSub = temp - double.Parse(output);
+                    output = outputTempSub.ToString();
+                    Output.Text = output;
+                    break;
+                
+                case "+":
+                    double outputTempAdd = temp + double.Parse(output);
+                    output = outputTempAdd.ToString();
+                    Output.Text = output;
+                    break;
+
+                case "*":
+                    double outputTempMul = temp * double.Parse(output);
+                    output = outputTempMul.ToString();
+                    Output.Text = output;
+                    break;
+
+                case "/":
+                    double outputTempDiv = temp / double.Parse(output);
+                    output = outputTempDiv.ToString();
+                    Output.Text = output;
+                    break;
+            }           
+        }
+
+
+        // Clear All Button
+        private void buttonClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            Output.Clear();
+            Output.Text = "0";
+            output = "";
+            PreviewOutput.Clear();
+            PreviewOutput.Text = "";
+            temp = 0;
+        }
+
+        // Delete Button
+        private void buttonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Output.Undo();
+        }
+
+        // Clear Button
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                Output.Clear();
+                Output.Text = "0";
+                output = "";
+                PreviewOutput.Clear();
+                PreviewOutput.Text = "";
+                temp = 0;
+            }
+            else
+            {
+                Output.Clear();
+                Output.Text = "0";
+                output = "";
+            }
+            
+            // Function for Concluded
+            if (PreviewOutput.Text.EndsWith("="))
+            {
+                Output.Clear();
+                Output.Text = "0";
+                output = "";
+                PreviewOutput.Clear();
+                PreviewOutput.Text = "";
+                temp = 0;
+            }
+        }
+
+        // Percentage Button
+        private void buttonPercent_Click(object sender, RoutedEventArgs e)
+        {      
+            /*
+            if (output != "")
+            {
+                temp = double.Parse(output);
+
+                output = "";
+
+                operation = "%";
+            }
+
+            switch (operation)
+            {
+                case "%":
+                    double outputTempPer = temp / double.Parse(output) * 100;
+                    output = outputTempPer.ToString();
+                    Output.Text = output;
+                    break;
+            }
+            PreviewOutput.Text = (temp.ToString() + " " + operation + " " + output);
+            */
+        }
+
+        // Copy Button
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Output.SelectAll();
+            Output.Copy();
+        }
+
+        // Square Root Button
+        private void buttonSquareRoot_Click(object sender, RoutedEventArgs e)
+        {            
+            if (output != "")
+            {
+                temp = double.Parse(output);
+
+                output = "";
+
+                operation = "√";
+            }
+
+            switch (operation)
+            {
+                case "√":
+                    double outputTempSqrt = Math.Sqrt(temp);
+                    output = outputTempSqrt.ToString();
+                    Output.Text = output;
+                    break;
+            }
+            PreviewOutput.Text = operation + "( " + temp + " )";
+        }
+
+        // Pozitive(+) - Negative(-)
+        private void buttonPoNe_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
